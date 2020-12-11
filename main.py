ce4b3891fb15f993dbb0c8dc189a5e2111de185b
Ansharah Ali <36322890+Ansharah123@users.noreply.github.com>
2020-12-11 13:32:56 +0500

    BasicPythoncode

---
+++ main.py
@@ -0,0 +1,157 @@
+
+#import math
+#This is also comment
+'''
+this is multiline comment
+'''
+
+# print("Hello world")
+# print("5+8")
+# print(math.gcd(3,6))
+
+#if(c<18):
+ #   print("hello")
+
+# a = 34
+# Harry = "Harry"
+# c = 45.36
+# print(a+c)
+# print(a*c)
+# print(a/c)
+
+# Wrong syntax
+# harry project = 45
+# Rules for creating variables
+
+#1. variable should start with a letter or an underscore
+#2. variable cannot start with a number
+#3. It can only contain alpha numeric character
+#4. Variable name are case sensitive harry and Harry are two different variables
+
+# typeA = type(a)
+# print(typeA)
+# typeB = type(Harry)
+# print(typeB)
+
+# a= "31"
+# # a = float(a)
+# print(type(a))
+# print(a)
+
+# name = "Harry,is,good"
+# #var =  name.lower()
+# var = name.upper()
+# var = name.replace(",","\n")
+# #print(var)
+ 
+# str2 = "Areeba love me "
+# str1 = "Areeba love me not "
+
+#print(str1 + str2)
+# name1 = "Harry"
+# name2 = "Rohan"
+# temp = "This is a {1} and he is a good boy named {0}".format(name1,name2)
+# temp2 = f"This is a {name1} and he is a good boy named {name2}"
+# print(temp2)
+# print(name.strip())
+# print(len(name))
+
+''' 
+Python Collection
+1.list
+2.Tuple
+3. Set
+4. Dictionar
+'''
+#List
+# lst = [61,2,3,4,6,41]
+# var = type(lst)
+# lst[2] = 65
+# var = lst[3]
+# var = lst[1:4]
+# lst.append(100)
+# lst.insert(1,100)
+# lst.pop()
+# del lst[3]
+# lst.clear()
+# print(lst)
+
+#Tuple
+# a = ("Areeba", "Namra", "Ansharah")
+# a = list(a)
+# var = a
+
+# a[0] = "Samana"
+# print(var)
+
+# #Set
+# s1 = {23,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,3,14,52,52}
+# s1.add(44444)
+# s1.update([12,12,343,432,532,532,54,1123])
+
+# #s1.remove(1999)
+# s1.discard(12764)
+# print(s1)
+
+#Dictionary
+# ha = {
+#     "Name":"Harry",
+#     "Class" : "4th",
+#     "Marks" : 34.23,
+#     "Hours In School": 6
+# }
+# print(ha)
+ #pop,clear,append
+# print(ha["Marks"])
+
+# age = input("Enter Your Age\n")
+# age = int(age)
+# if(age>18):
+#     print("You can drive a car")
+# elif(age==18):
+#     print("You are awesone")
+     
+# else:
+#     print("You cannot drive")
+
+#Loops:
+# for i in range(0 , 100 ):
+#     print(i)
+# li = [1, 4, "this"]
+
+# for item in li:
+#     print(item)
+
+# i=0
+# while(i<100):
+   
+#     if i==78:
+#         continue
+#     i = i + 1
+#     print(i+1)
+#Function
+# def greet():
+#     print("good morning sir")
+# greet()
+# def sum(a, b):
+#     c = a + b
+#     return c 
+
+# sum(3,4)
+# d = sum(3,4)
+# print(d)
+
+#oop
+# class Employee:
+#     def __init__(self,gname,gsalary):
+#         self.name = gname 
+#         self.salary = gsalary
+# harry = Employee("harry",34)
+# print(harry.name)
+# print(harry.salary)
+
+
+        
+
+
+    
